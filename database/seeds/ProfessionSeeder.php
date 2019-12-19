d8c966bba6cc862257f556179986e7ec29e8913f
raulrp3 <raul.ramirez@escuelaestech.es>
2019-12-19 10:51:10 +0100

    Creados seeders para generar datos de prueba

---
+++ database/seeds/ProfessionSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Profession;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProfessionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Profession::create([
+            'title' => 'Desarrollador back-end'
+        ]);
+
+        Profession::create([
+            'title' => 'Desarrollador front-end'
+        ]);
+        
+        Profession::create([
+            'title' => 'Diseñador web'
+        ]);
+
+        factory(Profession::class, 17)->create();
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\User;
+use App\Profession;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $professionId = Profession::where('title', 'Desarrollador back-end')->value('id');
+        $anotherProfessionId = Profession::where('title', 'Desarrollador front-end')->value('id');
+
+        User::create([
+            'name' => 'Raúl Ramírez Pérez',
+            'email' => 'raulramper003@gmail.com',
+            'password' => bcrypt('usuario'),
+            'profession_id' => $professionId,
+            'is_admin' => true
+        ]);
+
+        User::create([
+            'name' => 'Another user',
+            'email' => 'anotheruser@gmail.com',
+            'password' => bcrypt('usuario'),
+            'profession_id' => $anotherProfessionId,
+            'is_admin' => false
+        ]);
+
+        factory(User::class, 48)->create();
+    }
+}
