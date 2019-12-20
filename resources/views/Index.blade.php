36509847e2a53ee506699dd9fd08f9c28c6a5e5c
raulrp3 <raul.ramirez@escuelaestech.es>
2019-12-20 15:15:14 +0100

    Modificada vista principal

---
+++ resources/views/Index.blade.php
@@ -0,0 +1,8 @@
+@extends('Layout')
+
+@section('content')
+    <div class="jumbotron mt-5">
+        <h1 class="display-4">APP LARAVEL</h1>
+        <p class="lead">Aplicación backend con Laravel 5.5 y VueJs.</p>
+    </div>
+@endsection
